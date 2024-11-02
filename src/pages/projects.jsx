ac54ecee47382038bc99720f425b4cfc687599c6
Nic <3xpect1916@gmail.com>
2024-11-02 10:25:44 +0800

    Updated projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -39,21 +39,11 @@ const Projects = () => {
 					</div>
 					<div className="projects-container">
 						<div className="title projects-title">
-							Things I’ve made trying to put my dent in the
-							universe.
+							Projects that I have done or contributed to
 						</div>
 
 						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+							Throughout my studies at Nanyang Polytechnic, I have worked on various projects that have helped me to develop my skills in different programming languages as well as my problem-solving skills. Here are some of the projects that I have done or contributed to.
 						</div>
 
 						<div className="projects-list">
